ff92af80b6e09f3c650957be68aefb76dfcb58dd
hyedevj <hyedevj@gmail.com>
2023-08-03 15:00:03 +0900

    수정작업중

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+import Header from "./Header.js"
 import TodoForm from "./TodoForm.js"
 import TodoList from "./TodoList.js"
 
 export default function App({
     $target
 }) {
+    this.state = {
+        username: 'roto',
+        todos: []
+    }
+
+    new Header({
+        $target,
+        initialState: this.state.username
+    })
+
     new TodoForm({
         $target,
-        onSubmit: (content) => {
-            alert(`${content} 추가 처리!`)
+        onSubmit: async (content) => {
+            await request(`/${this.state.username}`), {
+                method: 'GET',
+                body: JSON.stringify({
+                    content,
+                    isCompleted: false
+                })
+            }
         }
     })
+
     const todoList = new TodoList({
-        $target,
-        initialState: DUMMY_DATA,
+        $target, 
+        initialState: this.state.todos,
         onToggle: (id) => {
             alert(`${id} 토글 예정`)
         },
@@ -20,4 +38,15 @@ export default function App({
             alert(`${id} 삭제 예정`)
         }
     })
+
+    const init = async () => {
+        const { username } = this.state
+
+        if (username) {
+            const todos = await request(`/${username}`)
+            console.log(todos)
+        }
+    }
+
+    init()
 }
